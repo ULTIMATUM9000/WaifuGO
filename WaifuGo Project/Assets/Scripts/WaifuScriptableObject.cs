e79dad76ae8df6c3dd58733d05231f43d2cd96e2
ULTIMATUM9000 <kylepogideleon@gmail.com>
2021-07-14 04:00:29 +0800

    small update

---
+++ WaifuGo Project/Assets/Scripts/WaifuScriptableObject.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Waifu", menuName = "WaifuScriptable")]
+public class WaifuScriptableObjet : ScriptableObject
+{
+    public Sprite WaifuSprite;
+
+    public string Name;
+}
